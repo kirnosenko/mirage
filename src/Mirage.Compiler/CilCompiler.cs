3afe7990536fd83dcc1af3053ea0062bd8a991b9
Semyon Kirnosenko <semyon.kirnosenko@gmail.com>
2013-10-14 21:11:49 +0400

    compile simple program with input and output

--- src/Mirage.Compiler/CilCompiler.cs
+++ src/Mirage.Compiler/CilCompiler.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Microsoft.Cci;
 using Microsoft.Cci.MutableCodeModel;
 
@@ -17,6 +18,16 @@ namespace Mirage.Compiler
 			using (var host = new PeReader.DefaultHost(nameTable))
 			{
 				var coreAssembly = host.LoadAssembly(host.CoreAssemblySymbolicIdentity);
+
+				IModule module = host.LoadUnitFrom("Mirage.dll") as IModule;
+				if (module == null || module is Dummy)
+				{
+					Console.WriteLine("Could not find");
+					return;
+				}
+				var machineType = module.GetAllTypes().First(x => x.Name.Value == "Machine");
+				var inputType = module.GetAllTypes().First(x => x.Name.Value == "ConsoleInput");
+				var outputType = module.GetAllTypes().First(x => x.Name.Value == "ConsoleOutput");
 
 				var assembly = new Assembly()
 				{
@@ -52,6 +63,9 @@ namespace Mirage.Compiler
 					Name = nameTable.GetNameFor("Program"),
 				};
 				rootUnitNamespace.Members.Add(programClass);
+				assembly.AllTypes.Add(machineType);
+				assembly.AllTypes.Add(inputType);
+				assembly.AllTypes.Add(outputType);
 				assembly.AllTypes.Add(programClass);
 				programClass.BaseClasses = new List<ITypeReference>() { host.PlatformType.SystemObject };
 
@@ -73,11 +87,71 @@ namespace Mirage.Compiler
 				var systemConsole = UnitHelper.FindType(nameTable, coreAssembly, "System.Console");
 				var writeLine = TypeHelper.GetMethod(systemConsole, nameTable.GetNameFor("WriteLine"), host.PlatformType.SystemString);
 
-				ilGenerator.Emit(OperationCode.Ldstr, "hello");
-				ilGenerator.Emit(OperationCode.Call, writeLine);
+				IMethodReference inputConstructor = new Microsoft.Cci.MethodReference(
+					host,
+					inputType,
+					CallingConvention.HasThis,
+					host.PlatformType.SystemVoid,
+					host.NameTable.Ctor,
+					0
+				);
+				var inputCast = TypeHelper.GetMethod(inputType, nameTable.GetNameFor("op_Implicit"), inputType);
+
+				IMethodReference outputConstructor = new Microsoft.Cci.MethodReference(
+					host,
+					outputType,
+					CallingConvention.HasThis,
+					host.PlatformType.SystemVoid,
+					host.NameTable.Ctor,
+					0
+				);
+				var outputCast = TypeHelper.GetMethod(outputType, nameTable.GetNameFor("op_Implicit"), outputType);
+
+				IMethodReference machineConstructor = new Microsoft.Cci.MethodReference(
+					host,
+					machineType,
+					CallingConvention.HasThis,
+					host.PlatformType.SystemVoid,
+					host.NameTable.Ctor,
+					0
+				);
+				var opIncHiPointer = TypeHelper.GetMethod(machineType, nameTable.GetNameFor("IncHiPointer"));
+				var opNot = TypeHelper.GetMethod(machineType, nameTable.GetNameFor("Not"));
+				var opInput = TypeHelper.GetMethod(machineType, nameTable.GetNameFor("Input"), inputCast.Type);
+				var opOutput = TypeHelper.GetMethod(machineType, nameTable.GetNameFor("Output"), outputCast.Type );
+
+				ilGenerator.Emit(OperationCode.Newobj, inputConstructor);
+				ilGenerator.Emit(OperationCode.Stloc_0);
+				ilGenerator.Emit(OperationCode.Newobj, outputConstructor);
+				ilGenerator.Emit(OperationCode.Stloc_1);
+				ilGenerator.Emit(OperationCode.Newobj, machineConstructor);
+				ilGenerator.Emit(OperationCode.Stloc_2);
+				ilGenerator.Emit(OperationCode.Ldloc_2);
+				ilGenerator.Emit(OperationCode.Callvirt, opIncHiPointer);
+				ilGenerator.Emit(OperationCode.Ldloc_2);
+				ilGenerator.Emit(OperationCode.Callvirt, opNot);
+				ilGenerator.Emit(OperationCode.Ldloc_2);
+				ilGenerator.Emit(OperationCode.Ldloc_0);
+				ilGenerator.Emit(OperationCode.Call, inputCast);
+				ilGenerator.Emit(OperationCode.Callvirt, opInput);
+				ilGenerator.Emit(OperationCode.Ldloc_2);
+				ilGenerator.Emit(OperationCode.Ldloc_1);
+				ilGenerator.Emit(OperationCode.Call, outputCast);
+				ilGenerator.Emit(OperationCode.Callvirt, opOutput);
 				ilGenerator.Emit(OperationCode.Ret);
 
-				var body = new ILGeneratorMethodBody(ilGenerator, true, 1, mainMethod, Enumerable<ILocalDefinition>.Empty, Enumerable<ITypeDefinition>.Empty);
+				var body = new ILGeneratorMethodBody(
+					ilGenerator,
+					true,
+					8,
+					mainMethod,
+					new List<ILocalDefinition>() {
+						new LocalDefinition() { Type = inputType },
+						new LocalDefinition() { Type = outputType },
+						new LocalDefinition() { Type = machineType },
+					},
+					Enumerable<ITypeDefinition>.Empty
+				);
 				mainMethod.Body = body;
 
 				using (var peStream = File.Create(exeName))
